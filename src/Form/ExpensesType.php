8995fab806cf26f18d16481bf2f37a0e35011d81
Brian <nairb725@gmail.com>
2021-11-17 14:13:19 +0100

    crud of Users and Expenses

---
+++ src/Form/ExpensesType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Expenses;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExpensesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('users')
+            ->add('Id_user')
+            ->add('Id_tricount')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Expenses::class,
+        ]);
+    }
+}
