91910cbca64a5eb869e2c61aaef9e8f31b3d5ca8
yann-cloud <obamayann9@gmail.com>
2021-11-18 14:00:14 +0100

    loggin page + security

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -6,11 +6,15 @@ use App\Repository\UsersRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UsersRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
-class Users
+
+ class Users implements UserInterface
 {
     /**
      * @ORM\Id
@@ -25,9 +29,30 @@ class Users
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=180, unique=true, nullable=false)
+     */
+    private $email;
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
      */
     private $password;
+
+     /**
+      * @ORM\Column(type="json")
+      */
+     private $roles = [];
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $activation_token;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $reset_token;
 
     /**
      * @ORM\ManyToMany(targetEntity=Tricount::class, mappedBy="User_tricount")
@@ -51,6 +76,11 @@ class Users
         $this->expenses = new ArrayCollection();
     }
 
+     public function __toString()
+     {
+         return $this->email;
+     }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -68,17 +98,32 @@ class Users
         return $this;
     }
 
-    public function getPassword(): ?string
+    public function getEmail(): ?string
     {
-        return $this->password;
+        return $this->email;
     }
 
-    public function setPassword(string $password): self
+    public function setEmail(string $email): self
     {
-        $this->password = $password;
+        $this->email = $email;
 
         return $this;
     }
+
+     /**
+      * @see UserInterface
+      */
+     public function getPassword(): string
+     {
+         return (string) $this->password;
+     }
+
+     public function setPassword(string $password): self
+     {
+         $this->password = $password;
+
+         return $this;
+     }
 
     /**
      * @return Collection|Tricount[]
@@ -160,4 +205,75 @@ class Users
 
         return $this;
     }
+
+    public function getActivationToken(): ?string
+    {
+        return $this->activation_token;
+    }
+
+    public function setActivationToken(?string $activation_token): self
+    {
+        $this->activation_token = $activation_token;
+
+        return $this;
+    }
+
+    public function getResetToken(): ?string
+    {
+        return $this->reset_token;
+    }
+
+    public function setResetToken(?string $reset_token): self
+    {
+        $this->reset_token = $reset_token;
+
+        return $this;
+    }
+
+     /**
+      * A visual identifier that represents this user.
+      *
+      * @see UserInterface
+      */
+     public function getUsername(): string
+     {
+         return (string) $this->email;
+     }
+
+     /**
+      * @see UserInterface
+      */
+     public function getRoles(): array
+     {
+         $roles = $this->roles;
+         // guarantee every user at least has ROLE_USER
+         $roles[] = 'ROLE_USER';
+
+         return array_unique($roles);
+     }
+
+     public function setRoles(array $roles): self
+     {
+         $this->roles = $roles;
+
+         return $this;
+     }
+
+     /**
+      * @see UserInterface
+      */
+     public function getSalt()
+     {
+         // not needed when using the "bcrypt" algorithm in security.yaml
+     }
+
+     /**
+      * @see UserInterface
+      */
+     public function eraseCredentials()
+     {
+         // If you store any temporary, sensitive data on the user, clear it here
+         // $this->plainPassword = null;
+     }
+
 }
